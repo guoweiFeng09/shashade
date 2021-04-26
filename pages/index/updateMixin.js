f5fb5f21cc4db0b0b6719317f374ede211630747
冯934520217 <934520217@qq.com>
2021-04-26 16:58:42 +0800

    更新 todo历史查看

---
+++ pages/index/updateMixin.js
@@ -0,0 +1,52 @@
+import updateView from '@/components/update'
+
+export default {
+	components: {
+		updateView
+	},
+	data() {
+		return {
+			update: {
+				isShow: false
+			}
+		}
+	},
+	onShow(e) {
+	
+	},
+	onHide() {
+	},
+	methods: {
+		checkUpdate() {
+			console.log('checkUpdate')
+			uniCloud.callFunction({
+			    name: 'first',
+			    data: { 
+					url: 'getVersion'
+				}
+			}).then(res => {
+				const {lastVerson, downloadUrl} = res.result
+				if(lastVerson > uni.appVersion) {
+					uni.showModal({
+					    title: '大人',
+					    content: '小的检测到有更新, 要不要更新呀',
+					    success:  res => {
+					        if (res.confirm) {
+								this.update.isShow = true
+								this.$nextTick(() => {
+									this.$refs.updateView.updateApp(downloadUrl)
+								})
+					        } else if (res.cancel) {
+					            console.log('用户点击取消')
+					        }
+					    }
+					})
+				}
+			}).catch(err => {
+				console.log('err', err)  
+			})
+		},
+			
+	}
+ 
+}
